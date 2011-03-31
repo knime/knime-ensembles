fb6fd64d30eb5b53ba1c7e694a73593843d6a3ac
Iris Adä <iris.adae@knime.com>
2011-03-31 15:41:22 +0000

    new table to pmml node

---
+++ org.knime.ensembles/src/org/knime/ensembles/tabletopmmlport/TableToPMMLNodeFactory.java
@@ -0,0 +1,35 @@
+package org.knime.ensembles.tabletopmmlport;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+public class TableToPMMLNodeFactory extends NodeFactory<TableToPMMLNodeModel> {
+
+	@Override
+	public TableToPMMLNodeModel createNodeModel() {
+		return new TableToPMMLNodeModel();
+	}
+
+	@Override
+	protected int getNrNodeViews() {
+		return 0;
+	}
+
+	@Override
+	public NodeView<TableToPMMLNodeModel> createNodeView(int viewIndex,
+			TableToPMMLNodeModel nodeModel) {
+		return null;
+	}
+
+	@Override
+	protected boolean hasDialog() {
+		return true;
+	}
+
+	@Override
+	protected NodeDialogPane createNodeDialogPane() {
+		return new TableToPMMLNodeDialog();
+	}
+
+}
---
+++ org.knime.ensembles/src/org/knime/ensembles/tabletopmmlport/TableToPMMLNodeModel.java
@@ -0,0 +1,114 @@
+package org.knime.ensembles.tabletopmmlport;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.knime.core.data.DataCell;
+import org.knime.core.data.DataColumnSpecCreator;
+import org.knime.core.data.DataRow;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.data.def.DefaultRow;
+import org.knime.core.data.model.PortObjectCell;
+import org.knime.core.data.xml.PMMLCell;
+import org.knime.core.data.xml.PMMLCellFactory;
+import org.knime.core.data.xml.PMMLValue;
+import org.knime.core.node.BufferedDataContainer;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.port.PortType;
+import org.knime.core.node.port.pmml.PMMLPortObject;
+import org.knime.core.node.port.pmml.PMMLPortObjectSpec;
+import org.knime.core.node.port.pmml.PMMLPortObjectSpecCreator;
+import org.knime.ensembles.tabletomodel.TableToModelNodeDialog;
+import org.w3c.dom.Document;
+
+public class TableToPMMLNodeModel extends NodeModel {
+	
+	 private final SettingsModelString m_column =
+	        TableToPMMLNodeDialog.createColumnModel();
+
+	protected TableToPMMLNodeModel() {
+		super(new PortType[] {new PortType(BufferedDataTable.class)},
+				new PortType[] {new PortType(PMMLPortObject.class)});
+	}
+	
+	@Override
+	protected PortObject[] execute(PortObject[] inObjects, ExecutionContext exec)
+			throws Exception {
+		 BufferedDataTable table = (BufferedDataTable) inObjects[0];
+	     Iterator<DataRow> it = table.iterator();
+	     int index = table.getSpec().findColumnIndex(m_column.getStringValue());
+	     PMMLCell model = (PMMLCell) it.next().getCell(index);
+	     Document doc = model.getDocument();
+
+	     PMMLPortObjectSpec spec = PMMLPortObject.parseSpec(doc);
+	     
+	     PMMLPortObject obj = new PMMLPortObject(spec, doc);
+
+	     return new PortObject[] {obj};
+	}
+	
+	
+	
+	@Override
+	protected PortObjectSpec[] configure(PortObjectSpec[] inSpecs)
+			throws InvalidSettingsException {
+		 DataTableSpec spec = (DataTableSpec) inSpecs[0];
+	        if (!spec.containsName(m_column.getStringValue())) {
+	            throw new InvalidSettingsException("Selected column '"
+	                    + m_column.getStringValue() + "' not in input spec.");
+	        }
+	        return new PortObjectSpec[]{null};
+	}
+
+	@Override
+	protected void loadInternals(File nodeInternDir, ExecutionMonitor exec)
+			throws IOException, CanceledExecutionException {
+		// nothing to load
+
+	}
+
+	@Override
+	protected void saveInternals(File nodeInternDir, ExecutionMonitor exec)
+			throws IOException, CanceledExecutionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void saveSettingsTo(NodeSettingsWO settings) {
+		m_column.saveSettingsTo(settings);
+
+	}
+
+	@Override
+	protected void validateSettings(NodeSettingsRO settings)
+			throws InvalidSettingsException {
+		m_column.validateSettings(settings);
+
+	}
+
+	@Override
+	protected void loadValidatedSettingsFrom(NodeSettingsRO settings)
+			throws InvalidSettingsException {
+		m_column.loadSettingsFrom(settings);
+
+	}
+
+	@Override
+	protected void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
